fbafc10c46dfba66a81fb7ecd04606189a1ef2ac
gravetir <dimadt505228@gmail.com>
2023-09-07 14:04:40 +0300

    Init3

---
+++ practise3/simple_guess.py
@@ -0,0 +1,21 @@
+#вывод корень числа , если он целый 
+# если корня нету тогда "Трудно"
+# def guess(num:int) -> int:
+def guess(num:int) -> int|str:
+    for i in range(num+1):
+        if num == i**2:
+            return(i)
+    return "Трудно"
+print(guess(int(input("Введите число: "))))
+# import math
+# res=int()
+# def guess(num:int) -> int:
+#     res=math.sqrt(num)
+#     return res
+# def guess(num:int) -> int|str:
+#     for i in range(1,num):
+#          if i**2==num:
+#              return(i)
+#          else:
+#              return("Трудно")
+#     print(guess(num))
